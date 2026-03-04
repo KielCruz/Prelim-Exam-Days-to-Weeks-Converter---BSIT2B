d723e51911b27e24b66ecde565051e6c0719083c
Von Laurernce <agustinovonlaurence@gmail.com>
2026-03-04 22:27:24 +0800

    Initial clean commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaystoWeeksConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number of days: ");
+
+            if (int.TryParse(Console.ReadLine(), out int totalDays))
+            {
+                int weeks = totalDays / 7;
+                int remainingDays = totalDays % 7;
+
+                Console.WriteLine($"Weeks: {weeks}");
+                Console.WriteLine($"Remaining days: {remainingDays}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input");
+            }
+        }
+    }
+}
